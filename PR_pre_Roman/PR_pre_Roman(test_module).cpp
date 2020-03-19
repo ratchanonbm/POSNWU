e719340fa13bb5605df23017fd47c88ed5e60b01
Ratchanon Panmas <qq.panmas@gmail.com>
2020-03-19 19:12:08 +0700

    PR_pre_Roman
    PR_pre_Roman

---
+++ PR_pre_Roman/PR_pre_Roman(test_module).cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int i=0,v=0,x=0,l=0,c=0;
+    while(n>=100){
+        cout<<"Loop c"<<endl;
+        c++;
+        n-=100;
+        cout<<"Now num: "<<n<<endl;
+    }
+
+    while(n>=50){
+        cout<<"Loop l"<<endl;
+        l++;
+        n-=50;
+        cout<<"Now num: "<<n<<endl;
+    }
+
+    while(n>=10){
+        cout<<"Loop x"<<endl;
+        x++;
+        n-=10;
+        cout<<"Now num: "<<n<<endl;
+    }
+
+    while(n>=5){
+        cout<<"Loop v"<<endl;
+        v++;
+        n-=5;
+        cout<<"Now num: "<<n<<endl;
+    }
+
+    while(n>=1){
+        cout<<"Loop i"<<endl;
+        i++;
+        n--;
+        cout<<"Now num: "<<n<<endl;
+    }
+    cout<<"Before If"<<endl;
+    cout<<"I = "<<i<<endl;
+    cout<<"V = "<<v<<endl;
+    cout<<"X = "<<x<<endl;
+    cout<<"L = "<<l<<endl;
+    cout<<"C = "<<c<<endl;
+
+    if(i>=4){
+        cout<<"If i"<<endl;
+        i-=3;
+        v++;
+    }
+    if(v>=2){
+        cout<<"If v"<<endl;
+        v-=2;
+        x++;
+    }
+    if(x>=3){
+        cout<<"If x"<<endl;
+        x-=5;
+        l++;
+    }
+    if(l>=2){
+        cout<<"If l"<<endl;
+        l-=2;
+        c++;
+    }
+    cout<<"After If"<<endl;
+    cout<<"I = "<<i<<endl;
+    cout<<"V = "<<v<<endl;
+    cout<<"X = "<<x<<endl;
+    cout<<"L = "<<l<<endl;
+    cout<<"C = "<<c<<endl;
+}
---
+++ PR_pre_Roman/PR_pre_Roman.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int d;
+    int sum_i=0,sum_v=0,sum_x=0,sum_l=0,sum_c=0;
+
+    cin>>d;
+
+    for(int p=1;p<=d;p++){
+        int n = p;
+
+        //Reset Roman Per Round
+        int i=0,v=0,x=0,l=0,c=0;
+
+        while(n>=100){
+            //For Debug
+            //cout<<"Loop c"<<endl;
+            c++;
+            n-=100;
+        }
+
+        while(n>=50){
+            //For Debug
+            //cout<<"Loop l"<<endl;
+            l++;
+            n-=50;
+        }
+
+        while(n>=10){
+            //For Debug
+            //cout<<"Loop x"<<endl;
+            x++;
+            n-=10;
+        }
+
+        while(n>=5){
+            //For Debug
+            //cout<<"Loop v"<<endl;
+            v++;
+            n-=5;
+        }
+
+        while(n>=1){
+            //For Debug
+            //cout<<"Loop i"<<endl;
+            i++;
+            n--;
+        }
+        //For Debug
+        //cout<<"Before If"<<endl;
+        //cout<<"I = "<<i<<endl;
+        //cout<<"V = "<<v<<endl;
+        //cout<<"X = "<<x<<endl;
+        //cout<<"L = "<<l<<endl;
+        //cout<<"C = "<<c<<endl;
+
+        if(i>=4){
+            //For Debug
+            //cout<<"If i"<<endl;
+            i-=3;
+            v++;
+        }
+        if(v>=2){
+            //For Debug
+            //cout<<"If v"<<endl;
+            v-=2;
+            x++;
+        }
+        if(x>=3){
+            //For Debug
+            //cout<<"If x"<<endl;
+            x-=5;
+            l++;
+        }
+        if(l>=2){
+            //For Debug
+            //cout<<"If l"<<endl;
+            l-=2;
+            c++;
+        }
+        //For Debug
+        //cout<<"After If"<<endl;
+        //cout<<"I = "<<i<<endl;
+        //cout<<"V = "<<v<<endl;
+        //cout<<"X = "<<x<<endl;
+        //cout<<"L = "<<l<<endl;
+        //cout<<"C = "<<c<<endl;
+
+        sum_i += i;
+        sum_v += v;
+        sum_x += x;
+        sum_l += l;
+        sum_c += c;
+
+        //For Debug
+        cout<<"Page:"<<p<<endl;
+        cout<<"I = "<<i<<endl;
+        cout<<"V = "<<v<<endl;
+        cout<<"X = "<<x<<endl;
+        cout<<"L = "<<l<<endl;
+        cout<<"C = "<<c<<endl;
+
+    }
+    //For Debug
+    //cout<<"Conclude: "<<endl;
+    //cout<<"I = "<<sum_i<<endl;
+    //cout<<"V = "<<sum_v<<endl;
+    //cout<<"X = "<<sum_x<<endl;
+    //cout<<"L = "<<sum_l<<endl;
+    //cout<<"C = "<<sum_c<<endl;
+    cout<<sum_i<<" "<<sum_v<<" "<<sum_x<<" "<<sum_l<<" "<<sum_c<<endl;
+
+}
