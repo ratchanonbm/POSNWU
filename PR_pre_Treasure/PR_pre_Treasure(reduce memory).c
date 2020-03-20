a7cf979efa7cd0c554f1ba2aac15232e5cd44e29
Ratchanon Panmas <qq.panmas@gmail.com>
2020-03-20 16:41:41 +0700

    Try to fix memory limit

--- PR_pre_Treasure/PR_pre_Treasure(reduce memory).c
+++ PR_pre_Treasure/PR_pre_Treasure(reduce memory).c
@@ -1,17 +1,18 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 int main() {
 
     char input[5];
     double x=0,y=0;
 
-    cin>>input;
+    scanf("%s",&input);
 
     while(input[0] != '*'){
 
-        short int dis = stoi(input);
+        short int dis = atoi(input);
         char dir[2];
 
         while(((int)input[0] < 65 || (int)input[0] > 90) && input[0] != '\0'){
@@ -29,14 +30,14 @@ int main() {
         else if(dir[0] == 'N' && dir[1] == 'E') {x+=sqrt(pow(dis,2)/2);y+=sqrt(pow(dis,2)/2);}
         else if(dir[0] == 'E' && dir[1] == '\0') {x+=dis;}
         else if(dir[0] == 'S' && dir[1] == 'E') {x+=sqrt(pow(dis,2)/2);y-=sqrt(pow(dis,2)/2);}
-        else if(dir[0] == 'S' && dir[1] == '\0') {y-=dis;}
+        else if(dir[0] == 'S' && dir[1] == '\0') {y-=dis;} //South
         else if(dir[0] == 'S' && dir[1] == 'W') {x-=sqrt(pow(dis,2)/2);y-=sqrt(pow(dis,2)/2);}
-        else if(dir[0] == 'W' && dir[1] == '\0') {x-=dis;}
+        else if(dir[0] == 'W' && dir[1] == '\0') {x-=dis;} //West
         else if(dir[0] == 'N' && dir[1] == 'W') {x-=sqrt(pow(dis,2)/2);y+=sqrt(pow(dis,2)/2);}
 
         scanf("%s",&input);
     }
 
-    cout<<x<<y<<setprecision(2);
-    cout<<sqrt(pow(x,2)+pow(y,2))<<setprecision(2);
+    printf("%.3lf %.3lf\n",x,y);
+    printf("%.3lf",sqrt(pow(x,2)+pow(y,2)));
 }
