c03d83483bc3e4fc3445db2261356faebd3c05d8
Ratchanon Panmas <qq.panmas@gmail.com>
2020-03-20 20:05:43 +0700

    Rewrite all Algorithm

--- PR_pre_Pattern/PR_pre_Pattern.cpp
+++ PR_pre_Pattern/PR_pre_Pattern.cpp
@@ -3,29 +3,37 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
-    int pattern[5000][70];
+    int N;
+    cin>>N;
 
-    int P,Q,R,maxP=0;
-    for(int i=0;i<n;i++){
+    int pat[5000][70];
+
+    int maxRow=0;
+
+    //Receive Data N line
+    for(int i=0;i<N;i++){
+        int P,Q,R;
         cin>>P>>Q>>R;
-        for(int j=0;j<R;j++){
-            pattern[P-1][(Q+j)-1] = 1;
-            //cout<<"Add pattern \tRow: "<<P<<"\t Col:"<<Q+j<<endl;
 
+        //Make Pattern Value to 2D array position
+        P--;
+        Q--;
+
+        //Mark The Pattern
+        for(int j=0;j<R;j++){
+            pat[P][Q+j] = 1;
         }
-        if(P>maxP) maxP = P;
+        if(P>maxRow) maxRow = P;
     }
 
-    //cout<<"Max Row: "<<maxP<<endl;
-
-    for(int i=0;i<maxP;i++){
+    //Print Pattern
+    for(int i=0;i<=maxRow;i++){
+        cout<<"Row - "<<i+1<<" | ";
         for(int j=0;j<70;j++){
-            //cout<<"Check Value: \tRow: "<<i<<"\tCol: "<<j<<"\tValue :"<<pattern[P][(Q+j)-1]<<endl;
-            if(pattern[i][j]==1) cout<<"x";
-            else cout<<"o";
+            if(pat[i][j]==1) cout<<"x"; //If mark print x
+            else cout<<"o"; //If not mark print o
         }
+        //End Row Newline
         cout<<endl;
     }
 
