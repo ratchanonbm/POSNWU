2281f97e4531de64abbecba41473b7f9197f4f5c
Ratchanon Panmas <qq.panmas@gmail.com>
2020-03-30 14:21:48 +0700

    Create 0019.cpp

---
+++ 0019/0019.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void best(int i){
+    if(i==0) return 1;
+    else return best(i-1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //Start Code
+    int n;
+    cin>>n;
+
+
+
+    return 0;
+}
