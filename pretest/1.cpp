b48d196a329518c113242212012a502ea3eecc0d
Ratchanon Panmas <qq.panmas@gmail.com>
2020-03-30 14:22:21 +0700

    Create 1.cpp

---
+++ pretest/1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //Start Code
+    cout<<pow(2,sizeof(unsigned long long)*sizeof(unsigned long long))-1;
+
+    return 0;
+}
