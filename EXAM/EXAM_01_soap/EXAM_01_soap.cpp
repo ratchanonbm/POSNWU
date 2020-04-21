449cafe2da31c3a727c757e34b50826669eaa476
Ratchanon Panmas <qq.panmas@gmail.com>
2020-04-22 00:54:55 +0700

    Update EXAM_01_soap.cpp

--- EXAM/EXAM_01_soap/EXAM_01_soap.cpp
+++ EXAM/EXAM_01_soap/EXAM_01_soap.cpp
@@ -7,6 +7,7 @@ CENTER: WU
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -17,23 +18,18 @@ int main(){
 
     int soap[N];
 
-    for(int i=0;i<N;i++) cin>>soap[i];
+    for(int i=0;i<N;i++) {
+        cin>>soap[i];
+    }
 
     sort(soap,soap+N);
 
-    //for(int i=0;i<N;i++) cout<<soap[i];
-
     int Q;
     cin>>Q;
-    for(int i=0;i<Q;i++){
-        int q;
-        int c = 0;
-        cin>>q;
-        for(int j=0;j<N;j++){
-            if(soap[j]<q) c++;
-             else break;
-        }
-        cout<<c<<endl;
+    for(int q=0;q<Q;q++){
+        int input;
+        cin>>input;
+        cout<<lower_bound(soap, soap+N, input) - soap<<endl;
     }
 
 
