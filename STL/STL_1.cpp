ff7a3171baa53d1c33d50141435060427a83bce5
Ratchanon Panmas <qq.panmas@gmail.com>
2020-04-21 23:13:46 +0700

    Create STL_1.cpp

---
+++ STL/STL_1.cpp
@@ -0,0 +1,18 @@
+/*
+    Task: NULL
+    Author: Ratchanon Panmas
+    Lang: C++
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //Driver Code
+    int arr[5] = {6,2,3,6,6};
+
+
+    return 0;
+}
